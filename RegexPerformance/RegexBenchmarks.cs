0de50c11a27adfcfeb0b72fe8ea8861202e7ddca
Damir Arh <damir.arh@gmail.com>
2024-03-31 18:33:12 +0200

    Add project files.

---
+++ RegexPerformance/RegexBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace RegexPerformance;
+
+public class RegexBenchmarks
+{
+    private readonly string validInput = "123.456.789";
+
+    [Benchmark]
+    public bool ValidateUsingSingleUseRegex()
+    {
+        return RegexValidator.ValidateUsingSingleUseRegex(validInput);
+    }
+
+    [Benchmark]
+    public bool ValidateUsingStaticRegex()
+    {
+        return RegexValidator.ValidateUsingStaticRegex(validInput);
+    }
+
+    [Benchmark]
+    public bool ValidateUsingStaticCompiledRegex()
+    {
+        return RegexValidator.ValidateUsingStaticCompiledRegex(validInput);
+    }
+
+    [Benchmark]
+    public bool ValidateUsingSourceGeneratedRegex()
+    {
+        return RegexValidator.ValidateUsingSourceGeneratedRegex(validInput);
+    }
+}
---
+++ RegexPerformance/RegexValidator.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexPerformance;
+
+public partial class RegexValidator
+{
+    private const string pattern = @"^[0-9]{3}\.[0-9]{3}\.[0-9]{3}$";
+
+    public static bool ValidateUsingSingleUseRegex(string input)
+    {
+        return Regex.IsMatch(input, pattern);
+    }
+
+    private static readonly Regex regex = new(pattern);
+
+    public static bool ValidateUsingStaticRegex(string input)
+    {
+        return regex.IsMatch(input);
+    }
+
+    private static readonly Regex compiledRegex = new(pattern, RegexOptions.Compiled);
+
+    public static bool ValidateUsingStaticCompiledRegex(string input)
+    {
+        return compiledRegex.IsMatch(input);
+    }
+
+    [GeneratedRegex(pattern)]
+    private static partial Regex SourceGeneratedRegex();
+
+    public static bool ValidateUsingSourceGeneratedRegex(string input)
+    {
+        return SourceGeneratedRegex().IsMatch(input);
+    }
+}
